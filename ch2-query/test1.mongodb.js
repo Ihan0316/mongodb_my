c14e5119ef64051d430da0b2f3a4fb17a306bbe7
“Ihan0316” <aijnjj12@gmail.com>
2025-02-25 10:20:48 +0900

    25/2/25 2교시

---
+++ ch2-query/test1.mongodb.js
@@ -0,0 +1,23 @@
+db.by_road_type.find({ county: "강릉시" }, { "교차로내.accident_count": 1 });
+db.by_road_type.find(
+  { "기타단일로.death_toll": 0 },
+  { city_or_province: 1, county: 1 }
+);
+
+db.by_type.find(
+  { $and: [{ type: "차대차" }, { death_tole: 0 }, { accident_count: 100 }] },
+  { county: 1 }
+);
+
+db.stores.insert([
+  { _id: 1, name: "Java Hut", description: "Coffee and cakes" },
+  { _id: 2, name: "Burger Buns", description: "Gourmet hamburgers" },
+  { _id: 3, name: "Coffee Shop", description: "Just coffee" },
+  { _id: 4, name: "Clothes Clothes Clothes", description: "Discount clothing" },
+  { _id: 5, name: "Java Shopping", description: "Indonesian goods" },
+]);
+
+//Text index
+db.stores.createIndex({ name: "text", description: "text" });
+
+db.stores.find({ $text: { $search: "java coffee shop" } });
